fc2f31acf2b68a29a97fb46fa58d8c2fba0a3a03
sebastientrouillet-design <sebastien.trouillet@tiime.fr>
2026-03-26 13:33:27 +0100

    Update style-dictionary.config.js

--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -1,20 +1,46 @@
-export default {
-  source: ['tokens/**/*.json'],
-  platforms: {
-    web: {
-      transformGroup: 'css',
-      buildPath: 'build/web/',
-      files: [{ destination: 'variables.css', format: 'css/variables' }]
-    },
-    ios: {
-      transformGroup: 'ios-swift',
-      buildPath: 'build/ios/',
-      files: [{ destination: 'DesignTokens.swift', format: 'ios-swift/class.swift', className: 'DesignTokens' }]
-    },
-    android: {
-      transformGroup: 'android',
-      buildPath: 'build/android/',
-      files: [{ destination: 'tokens.xml', format: 'android/resources' }]
-    }
-  }
-};
+import StyleDictionary from 'style-dictionary';
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const tokenFiles = await fs.readdir('./tokens');
+
+const configs = tokenFiles
+  .filter(f => f.endsWith('.json'))
+  .map(file => {
+    const name = path.basename(file, '.json');
+    return {
+      source: [`tokens/${file}`],
+      platforms: {
+        web: {
+          transformGroup: 'css',
+          buildPath: 'build/web/',
+          files: [{
+            destination: `${name}.css`,
+            format: 'css/variables',
+          }]
+        },
+        ios: {
+          transformGroup: 'ios-swift',
+          buildPath: 'build/ios/',
+          files: [{
+            destination: `${name}.swift`,
+            format: 'ios-swift/class.swift',
+            className: name.replace(/[^a-zA-Z0-9]/g, '_'),
+          }]
+        },
+        android: {
+          transformGroup: 'android',
+          buildPath: 'build/android/',
+          files: [{
+            destination: `${name}.xml`,
+            format: 'android/resources',
+          }]
+        }
+      }
+    };
+  });
+
+for (const config of configs) {
+  const sd = new StyleDictionary(config);
+  await sd.buildAllPlatforms();
+}
