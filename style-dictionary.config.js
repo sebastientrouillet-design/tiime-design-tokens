a3c675f21feadd88b20aa2989568e956cd9b77de
sebastientrouillet-design <sebastien.trouillet@tiime.fr>
2026-03-26 10:48:56 +0100

    Create style-dictionary.config.js

---
+++ style-dictionary.config.js
@@ -0,0 +1,42 @@
+const StyleDictionary = require('style-dictionary');
+
+module.exports = {
+  source: ['tokens/**/*.json'],
+  platforms: {
+
+    web: {
+      transformGroup: 'css',
+      buildPath: 'build/web/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        }
+      ]
+    },
+
+    ios: {
+      transformGroup: 'ios-swift',
+      buildPath: 'build/ios/',
+      files: [
+        {
+          destination: 'DesignTokens.swift',
+          format: 'ios-swift/class.swift',
+          className: 'DesignTokens',
+        }
+      ]
+    },
+
+    android: {
+      transformGroup: 'android',
+      buildPath: 'build/android/',
+      files: [
+        {
+          destination: 'tokens.xml',
+          format: 'android/resources',
+        }
+      ]
+    },
+
+  }
+};
